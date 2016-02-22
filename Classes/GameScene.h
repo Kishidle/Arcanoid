31c6e82cb9395e012d28ff0e3da75b65fbf50e20
VN <arcaramon@gmail.com>
2016-02-22 18:08:36 +0800

    Revert "updates to GameScene"
    This reverts commit 6ba5faa8f18288a3ebbb001cb86ee14c02e71bc0.

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -1,24 +1,11 @@
 #pragma once
 #include "cocos2d.h"
 #include "SimpleAudioEngine.h"
-#include <map>
-#include <vector>
+
 class GameScene : public cocos2d::Layer {
 public:
 	static cocos2d::Scene* createScene();
-	virtual bool init();
-
-	void update(float) override;
-	bool isKeyPressed(cocos2d::EventKeyboard::KeyCode);
-	void playerMovement(cocos2d::EventKeyboard::KeyCode);
-
+	virtual bool init() override;
 	CREATE_FUNC(GameScene);
-private:
-	char curDir;
-	cocos2d::Sprite* paddle;
-	cocos2d::Vec2 pos;
-	static std::map<cocos2d::EventKeyboard::KeyCode, std::chrono::high_resolution_clock::time_point> keys;
-	
-public: 
-	
+	void update(float) override;
 };
