807e1fc0c6e57989ea18d2bbac65de4591603222
VN <arcaramon@gmail.com>
2016-02-22 17:58:06 +0800

    updates to GameScene
    the paddle should move properly now, but it's still going out of the
    play area

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -14,11 +14,69 @@ Scene* GameScene::createScene() {
 bool GameScene::init() {
 	//code here LOL
 
-	auto paddle = Sprite::create("paddle.png");
+	paddle = Sprite::create("paddle.png");
+	paddle->setPosition(200, 200);
+	paddle->setAnchorPoint(Vec2(0, 0));
 	this->addChild(paddle);
+	curDir = 'x';
+	
+	auto kbListener = EventListenerKeyboard::create();
+	
+	Director::getInstance()->getOpenGLView()->setIMEKeyboardState(true);
+	kbListener->onKeyPressed = [=](EventKeyboard::KeyCode keyCode, Event* event) {
+		if (keys.find(keyCode) == keys.end()) {
+			keys[keyCode] == std::chrono::high_resolution_clock::now();
+		}
+		return true;
+	};
+	kbListener->onKeyReleased = [=](EventKeyboard::KeyCode keyCode, Event* event) {
+		keys.erase(keyCode);
+	};
+
+
+	this->_eventDispatcher->addEventListenerWithSceneGraphPriority(kbListener, paddle);
+	this->scheduleUpdate();
 	return true;
+
+
+}
+bool GameScene::isKeyPressed(EventKeyboard::KeyCode keyCode) {
+	if (keys.find(keyCode) != keys.end())
+		return true;
+	return false;
+}
+
+void GameScene::playerMovement(EventKeyboard::KeyCode keyCode) {
+	pos = paddle->getPosition();
+	if (isKeyPressed(EventKeyboard::KeyCode::KEY_LEFT_ARROW)) {
+		curDir = 'a';
+			
+	}
+	else if (isKeyPressed(EventKeyboard::KeyCode::KEY_RIGHT_ARROW)) {
+		curDir = 'd';
+	}
+	else curDir = 'n';
+	
 }
 
 void GameScene::update(float delta) {
+	pos = paddle->getPosition();
+	Node::update(delta);
+	if (isKeyPressed(EventKeyboard::KeyCode::KEY_LEFT_ARROW)) {
+		curDir = 'a';
+	}
+	else if (isKeyPressed(EventKeyboard::KeyCode::KEY_RIGHT_ARROW)) {
+		curDir = 'd';
+	}
+	else curDir = 'n';
 
+	if (curDir == 'a') {
+		pos.x = pos.x - 150 * delta;
+	}
+	if (curDir == 'd') {
+		pos.x = pos.x + 150 * delta;
+	}
+	paddle->setPosition(pos);
 }
+std::map<cocos2d::EventKeyboard::KeyCode,
+	std::chrono::high_resolution_clock::time_point> GameScene::keys;
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "cocos2d.h"
 #include "SimpleAudioEngine.h"
-
+#include <map>
+#include <vector>
 class GameScene : public cocos2d::Layer {
 public:
 	static cocos2d::Scene* createScene();
-	virtual bool init() override;
-	CREATE_FUNC(GameScene);
+	virtual bool init();
+
 	void update(float) override;
+	bool isKeyPressed(cocos2d::EventKeyboard::KeyCode);
+	void playerMovement(cocos2d::EventKeyboard::KeyCode);
+
+	CREATE_FUNC(GameScene);
+private:
+	char curDir;
+	cocos2d::Sprite* paddle;
+	cocos2d::Vec2 pos;
+	static std::map<cocos2d::EventKeyboard::KeyCode, std::chrono::high_resolution_clock::time_point> keys;
+	
+public: 
+	
 };
